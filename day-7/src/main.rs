c5da4e749fca63994dced8a54a181a7efd2defbb
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-12-08 22:01:26 +0200

    Add 7.1 solution

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,7 +1,104 @@
 use reader;
 
+#[derive(Debug, Clone)]
+enum FileSystem {
+    File {
+        size: usize,
+        name: String,
+    },
+    Folder {
+        files: Vec<FileSystem>,
+        name: String,
+        size: usize,
+    },
+}
+
+fn parse_folder(logs: &mut Vec<String>, name: String) -> FileSystem {
+    // Recursively parse file system contents
+    let mut current_files: Vec<FileSystem> = vec![];
+    let mut total_size: usize = 0;
+
+    loop {
+        let line = logs.pop();
+
+        if let Some(value) = line {
+            if value.starts_with("$ cd ..") {
+                break;
+            } else if value.starts_with("$ cd") {
+                // Recursively parse subfolder
+                let mut folder_name = value
+                    .split(" ")
+                    .map(|v| v.to_string())
+                    .collect::<Vec<String>>();
+                let f = parse_folder(logs, folder_name.pop().unwrap());
+
+                if let FileSystem::Folder { files, name, size } = f.clone() {
+                    total_size += size;
+                    current_files.push(f);
+                } else {
+                    panic!("Not a folder: {:?}", f);
+                }
+            } else if value.as_bytes()[0].is_ascii_digit() {
+                // Parse file
+                let mut f = value
+                    .split(" ")
+                    .map(|v| v.to_string())
+                    .collect::<Vec<String>>();
+                let s = f[0].parse::<usize>().unwrap();
+                total_size += s;
+                current_files.push(FileSystem::File {
+                    size: s,
+                    name: f.pop().unwrap(),
+                });
+            } else {
+                /*
+                Starts with:
+                "dir <dirname>"
+                "$ ls"
+                */
+            }
+        } else {
+            // EOF
+            break;
+        }
+    }
+
+    FileSystem::Folder {
+        files: current_files,
+        name,
+        size: total_size,
+    }
+}
+
+fn parse_fs(input: reader::Reader) -> FileSystem {
+    // Starting point for parsing the structure of file system.
+    let mut lines = input.lines();
+    lines.reverse();
+    lines.pop(); // Always "$ cd /"
+    parse_folder(&mut lines, "/".to_string())
+}
+
+fn traverse_fs(fs: &FileSystem) -> usize {
+    let mut res = 0;
+
+    if let FileSystem::Folder { files, name, size } = fs {
+        if size <= &100_000 {
+            res += size;
+        }
+
+        for file in files.iter() {
+            match file {
+                FileSystem::Folder { files, name, size } => res += traverse_fs(file),
+                _ => {}
+            }
+        }
+    }
+
+    res
+}
+
 fn part_one(input: reader::Reader) -> usize {
-    0
+    traverse_fs(&parse_fs(input))
 }
 
 fn part_two(input: reader::Reader) -> usize {
@@ -22,18 +119,17 @@ fn input() -> reader::Reader {
 
 #[test]
 fn test_part_one_example() {
-    assert_eq!(part_one(get_test_input()), 0);
+    assert_eq!(part_one(get_test_input()), 95437);
 }
 
-#[ignore]
 #[test]
 fn test_part_one() {
-    assert_eq!(part_one(input()), 0);
+    assert_eq!(part_one(input()), 1086293);
 }
 
 #[test]
 fn test_part_two_example() {
-    assert_eq!(part_two(get_test_input()), 0);
+    assert_eq!(part_two(get_test_input()), 8381165);
 }
 
 #[ignore]
