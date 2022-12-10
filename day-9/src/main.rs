e593b054d61bd63d0fa4e8ea62b9e07e80d64dc5
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-12-10 09:42:45 +0200

    Solve 9.1

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,7 +1,125 @@
 use reader;
+use std::collections::{HashSet, LinkedList};
+use std::ops;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl ops::Add<Position> for Position {
+    type Output = Position;
+
+    fn add(self, rhs: Position) -> Position {
+        Position {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl ops::Sub<Position> for Position {
+    type Output = Position;
+
+    fn sub(self, rhs: Position) -> Position {
+        Position {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl Position {
+    fn as_follow_direction(self) -> Position {
+        let mut y = self.y;
+        let mut x = self.x;
+
+        if self.y == 2 {
+            y = 1;
+        } else if self.y == -2 {
+            y = -1;
+        } else if self.x == 2 {
+            x = 1;
+        } else if self.x == -2 {
+            x = -1;
+        } else {
+            x = 0;
+            y = 0;
+        }
+
+        Position { x, y }
+    }
+}
+
+struct Rope {
+    positions: HashSet<Position>,
+    position: Position,
+}
+
+impl Rope {
+    fn new() -> Rope {
+        Rope {
+            positions: HashSet::from([Position { x: 0, y: 0 }]),
+            position: Position { x: 0, y: 0 },
+        }
+    }
+
+    fn from(length: usize) -> LinkedList<Rope> {
+        let mut res = LinkedList::new();
+
+        (0..length).for_each(|_| res.push_back(Rope::new()));
+
+        res
+    }
+
+    fn move_to_dir(&mut self, dir: Position) {
+        self.position = self.position + dir;
+        self.positions.insert(self.position);
+    }
+
+    fn move_towards(&mut self, pos: Position) {
+        let dir = (pos - self.position).as_follow_direction();
+        self.move_to_dir(dir);
+    }
+}
+
+fn move_rope(rope: &mut LinkedList<Rope>, dir: Position) {
+    let head_pos = rope.front().unwrap().position;
+    let mut target = head_pos + dir;
+
+    rope.front_mut().unwrap().move_to_dir(dir);
+
+    rope.iter_mut().skip(1).for_each(|r| {
+        r.move_towards(target);
+        target = r.position;
+    })
+}
+
+fn execute_lines(rope: &mut LinkedList<Rope>, lines: &Vec<String>) {
+    lines.iter().for_each(|line| {
+        let (dir, amount) = line.split_once(" ").unwrap();
+
+        let dir = match dir {
+            "U" => Position { x: 0, y: 1 },
+            "D" => Position { x: 0, y: -1 },
+            "L" => Position { x: -1, y: 0 },
+            "R" => Position { x: 1, y: 0 },
+            _ => Position { x: 0, y: 0 },
+        };
+
+        for _ in 0..amount.parse::<usize>().unwrap() {
+            move_rope(rope, dir);
+        }
+    })
+}
 
 fn part_one(input: reader::Reader) -> usize {
-    0
+    let lines = input.lines();
+    let mut rope = Rope::from(2);
+    execute_lines(&mut rope, &lines);
+
+    rope.back().unwrap().positions.len()
 }
 
 fn part_two(input: reader::Reader) -> usize {
@@ -22,13 +140,12 @@ fn input() -> reader::Reader {
 
 #[test]
 fn test_part_one_example() {
-    assert_eq!(part_one(get_test_input()), 0);
+    assert_eq!(part_one(get_test_input()), 13);
 }
 
-#[ignore]
 #[test]
 fn test_part_one() {
-    assert_eq!(part_one(input()), 0);
+    assert_eq!(part_one(input()), 6067);
 }
 
 #[test]
