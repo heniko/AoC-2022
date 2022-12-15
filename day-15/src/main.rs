bd02eecd1fa89f34a26f704017a045a7b0eb1e41
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2022-12-15 13:16:06 +0200

    Solve 15.1

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,7 +1,117 @@
 use reader;
+use regex::Regex;
+use std::cmp::max;
+use std::{str::FromStr, string::ParseError};
 
-fn part_one(input: reader::Reader) -> usize {
-    0
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct Range {
+    start: isize,
+    end: isize,
+}
+
+impl Range {
+    fn from(start: isize, end: isize) -> Range {
+        Range { start, end }
+    }
+
+    fn len(&self) -> isize {
+        self.end - self.start
+    }
+
+    fn common(&self, other_end: isize) -> isize {
+        if self.end <= other_end {
+            self.len()
+        } else if self.start <= other_end {
+            // Range end is inclusive
+            other_end - self.start
+        } else {
+            0
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn from(x: isize, y: isize) -> Point {
+        Point { x, y }
+    }
+
+    fn manhattan_distance(&self, other: Point) -> isize {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+struct Sensor {
+    at: Point,
+    beacon: Point,
+}
+
+impl FromStr for Sensor {
+    type Err = ParseError;
+
+    fn from_str(str: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(
+            r"^Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)$",
+        )
+        .unwrap();
+
+        let caps = re.captures(str).unwrap();
+
+        Ok(Sensor {
+            at: Point::from(
+                caps[1].parse::<isize>().unwrap(),
+                caps[2].parse::<isize>().unwrap(),
+            ),
+            beacon: Point::from(
+                caps[3].parse::<isize>().unwrap(),
+                caps[4].parse::<isize>().unwrap(),
+            ),
+        })
+    }
+}
+
+impl Sensor {
+    fn closest_len(&self) -> isize {
+        self.at.manhattan_distance(self.beacon)
+    }
+
+    fn range_at_row(&self, y: isize) -> Option<Range> {
+        // Returns the range of cover.
+        let extras = self.closest_len() - self.at.manhattan_distance(Point { x: self.at.x, y });
+
+        if extras < 0 {
+            None
+        } else {
+            Some(Range::from(self.at.x - extras, self.at.x + extras))
+        }
+    }
+}
+
+fn part_one(input: reader::Reader, y: isize) -> isize {
+    let mut ranges = input
+        .lines_as::<Sensor>()
+        .iter()
+        .map(|sensor| sensor.range_at_row(y))
+        .filter(|value| value != &Option::None)
+        .map(|value| value.unwrap())
+        .collect::<Vec<Range>>();
+
+    ranges.sort_by(|a, b| a.start.cmp(&b.start));
+
+    let mut len = 0;
+    let mut max_end = -2_000_000_000;
+
+    for range in ranges.iter() {
+        len += range.len() - range.common(max_end);
+        max_end = max(max_end, range.end);
+    }
+
+    len
 }
 
 fn part_two(input: reader::Reader) -> usize {
@@ -11,7 +121,7 @@ fn part_two(input: reader::Reader) -> usize {
 fn main() {
     println!(
         "Day x\n\tPart 1: {:?}\n\tPart 2: {:?}",
-        part_one(input()),
+        part_one(input(), 2_000_000),
         part_two(input())
     );
 }
@@ -22,13 +132,12 @@ fn input() -> reader::Reader {
 
 #[test]
 fn test_part_one_example() {
-    assert_eq!(part_one(get_test_input()), 26);
+    assert_eq!(part_one(get_test_input(), 10), 26);
 }
 
-#[ignore]
 #[test]
 fn test_part_one() {
-    assert_eq!(part_one(input()), 0);
+    assert_eq!(part_one(input(), 2_000_000), 4876693);
 }
 
 #[test]
